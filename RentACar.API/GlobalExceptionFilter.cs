ddea29ae4a62f4491462021f897abbfd26f07bf1
Tadeusz Jerzy Z <tadeo13@gmail.com>
2021-04-22 12:04:59 +0200

    Enhanced global exception filter

--- RentACar.API/GlobalExceptionFilter.cs
+++ RentACar.API/GlobalExceptionFilter.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using RentACar.Core.Exceptions;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net;
 
 
@@ -11,36 +13,45 @@ namespace RentACar.API
         public void OnException(ExceptionContext context)
         {
             // custom handling for domain exceptions
-            if (context.Exception is BusinessException businessException)
-            {
-                var statusCode = businessException switch
-                {
-                    EntityNotFoundException e => 404,
-                    _ => 400
-                };
-
-                var typeName = context.Exception.GetType().Name;
-                var exception = businessException;
-                var details = new
-                {
-                    Status = 400,
-                    Title = "Bad Request",
-                    Message = exception.Message,
-                    BusinessException = typeName,
-                    Trace = context.HttpContext.TraceIdentifier
-                };
-
-                var json = new
-                {
-                    errors = new[] { details }
-                };
-
-                context.Result = new BadRequestObjectResult(json);
-                context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                context.ExceptionHandled = true;
-            } 
+            if (!(context.Exception is BusinessException businessException))
+                return;
             
+            int statusCode = businessException switch
+            {
+                EntityNotFoundException e => 404,
+                _ => 400
+            };
 
+            string errorType = businessException switch
+            {
+                EntityNotFoundException e => "Not Found",
+                _ => "Bad Request"
+            };
+
+            var typeName = context.Exception.GetType().Name;
+            
+            var json = new
+            {
+                // match default asp core validation errors structure
+                type = errorType, 
+                title = $"An exception {typeName} occurred",
+                status = statusCode,
+                traceId = Activity.Current?.Id ?? context.HttpContext?.TraceIdentifier, // https://stackoverflow.com/a/61395255
+                errors = new Dictionary<string, object>() 
+                {
+                    {  typeName ,  new string[] { businessException.Message } } 
+                } 
+                
+            };
+
+            context.Result = statusCode switch
+            {
+                404 => new NotFoundObjectResult(json),
+                _ => new BadRequestObjectResult(json)
+            };
+
+            context.HttpContext.Response.StatusCode = statusCode;
+            context.ExceptionHandled = true;
 
         }
     }
